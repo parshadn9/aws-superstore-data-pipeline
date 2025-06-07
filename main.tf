212ec3897a7f07338efd04e68f62a27d6a34cca6
Parshad Nirbhavane <parshadn9@gmail.com>
2025-06-07 22:23:17 +1000

    updated

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-# --- S3 Bucket for Superstore Data and Athena Logs ---
+provider "aws" {
+  region = var.aws_region
+}
+
+# --- S3 Bucket for Data Storage ---
 resource "aws_s3_bucket" "superstore_bucket" {
   bucket = var.bucket_name
 
@@ -8,11 +12,10 @@ resource "aws_s3_bucket" "superstore_bucket" {
   }
 }
 
-# Create Athena logs folder as an empty object (recommended method)
-resource "aws_s3_object" "athena_logs_folder" {
-  bucket  = aws_s3_bucket.superstore_bucket.id
-  key     = "athena_logs/"
-  content = ""
+# --- S3 Object for Athena logs (folder simulation) ---
+resource "aws_s3_bucket_object" "athena_logs_folder" {
+  bucket = aws_s3_bucket.superstore_bucket.id
+  key    = "athena_logs/"  # This simulates a folder inside the bucket
 }
 
 # --- Glue Catalog Database ---
@@ -26,26 +29,55 @@ resource "aws_iam_role" "glue_service_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "glue.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "glue.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-# Attach Glue Managed Policy to Glue Role
-resource "aws_iam_role_policy_attachment" "glue_policy_attachment" {
+# --- Attach Glue Service Policy to IAM Role ---
+resource "aws_iam_role_policy_attachment" "glue_service_policy" {
   role       = aws_iam_role.glue_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
 
-# --- Attach Admin Policy to Existing IAM User ---
-resource "aws_iam_user_policy_attachment" "luffyonepiece_admin" {
-  user       = var.iam_user_name
+# --- Glue Crawler to scan S3 data ---
+resource "aws_glue_crawler" "superstore_crawler" {
+  name          = "superstore-crawler"
+  role          = aws_iam_role.glue_service_role.arn
+  database_name = aws_glue_catalog_database.superstore_db.name
+  table_prefix  = "superstore_"
+
+  s3_target {
+    path = "s3://${var.bucket_name}/raw/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0,
+    CrawlerOutput = {
+      Partitions = {
+        AddOrUpdateBehavior = "InheritFromTable"
+      }
+    }
+  })
+
+  depends_on = [
+    aws_glue_catalog_database.superstore_db,
+    aws_s3_bucket.superstore_bucket
+  ]
+}
+
+# --- IAM User Resource (Optional, if needed) ---
+resource "aws_iam_user" "admin_user" {
+  name = var.iam_user_name
+}
+
+# --- IAM Policy Attachment to IAM User ---
+resource "aws_iam_user_policy_attachment" "admin_user_policy" {
+  user       = aws_iam_user.admin_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
--- variable.tf
+++ variable.tf
@@ -1,27 +1,32 @@
+# AWS Region
 variable "aws_region" {
   description = "The AWS region where resources will be deployed"
   type        = string
   default     = "ap-southeast-2"
 }
 
+# S3 Bucket Name
 variable "bucket_name" {
   description = "The name of the S3 bucket to create or manage"
   type        = string
   default     = "luffy-superstore-bucket-2025"
 }
 
+# Glue Database Name
 variable "glue_database_name" {
   description = "The name of the Glue catalog database"
   type        = string
   default     = "superstore_db"
 }
 
+# Glue IAM Role Name
 variable "glue_role_name" {
   description = "The name of the IAM role for AWS Glue"
   type        = string
   default     = "AWSGlueServiceRole-luffyhour"
 }
 
+# IAM User Name for Policy Attachment
 variable "iam_user_name" {
   description = "The name of the existing IAM user to attach policies to"
   type        = string
