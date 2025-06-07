fc6b7ddbdfbfa09ef67bfb05ede61812b5f0b049
Parshad Nirbhavane <parshadn9@gmail.com>
2025-06-07 14:45:14 +1000

    updated

--- main.tf
+++ main.tf
@@ -1,29 +1,41 @@
-# IAM user
-resource "aws_iam_user" "admin_user" {
-  name = var.iam_user_name
+
+
+# --- S3 Bucket for Storing Data and Athena Logs ---
+resource "aws_s3_bucket" "superstore_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "Superstore Data Bucket"
+    Environment = "Dev"
+  }
 }
 
+# --- Athena Logs Folder ---
+resource "aws_s3_object" "athena_logs" {
+  bucket  = aws_s3_bucket.superstore_bucket.bucket
+  key     = "athena_logs/"
+  content = "" # Required to create an empty object
+}
+
+# --- Use Existing IAM User (Import Before Applying) ---
+resource "aws_iam_user" "admin_user" {
+  name = "onepieceluffy"
+}
+
+# --- Attach Admin Policy to Existing User ---
 resource "aws_iam_user_policy_attachment" "admin_access" {
   user       = aws_iam_user.admin_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-# S3 bucket and folders
-resource "aws_s3_bucket" "superstore_bucket" {
-  bucket = var.bucket_name
-  tags = {
-    Name = "Superstore Data Bucket"
-  }
+# --- Glue Catalog Database ---
+resource "aws_glue_catalog_database" "superstore_db" {
+  name = var.glue_database_name
 }
 
-resource "aws_s3_bucket_object" "athena_logs" {
-  bucket = aws_s3_bucket.superstore_bucket.id
-  key    = "athena_logs/"
-}
-
-# IAM role for Glue
-resource "aws_iam_role" "glue_role" {
-  name = var.glue_role_name
+# --- IAM Role for Glue Service ---
+resource "aws_iam_role" "glue_service_role" {
+  name = "AWSGlueServiceRole-luffyhour"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -37,46 +49,34 @@ resource "aws_iam_role" "glue_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "glue_s3_access" {
-  role       = aws_iam_role.glue_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "glue_service" {
-  role       = aws_iam_role.glue_role.name
+# --- Attach Glue Service Policy ---
+resource "aws_iam_role_policy_attachment" "glue_service_policy" {
+  role       = aws_iam_role.glue_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
 
-# Glue catalog database
-resource "aws_glue_catalog_database" "catalog_db" {
-  name = var.glue_db_name
-}
-
-# Glue crawler
+# --- AWS Glue Crawler ---
 resource "aws_glue_crawler" "crawler" {
-  name          = var.glue_crawler_name
-  role          = aws_iam_role.glue_role.arn
-  database_name = aws_glue_catalog_database.catalog_db.name
-  schedule      = "cron(0 * * * ? *)"
+  name          = "luffyhourly"
+  role          = aws_iam_role.glue_service_role.arn
+  database_name = aws_glue_catalog_database.superstore_db.name
 
   s3_target {
-    path = "s3://${var.bucket_name}/orders/"
+    path = "s3://${aws_s3_bucket.superstore_bucket.bucket}/data/"
   }
 
-  configuration = jsonencode({
-    Version = 1.0,
-    CrawlerOutput = {
-      UpdateBehavior = "UPDATE_IN_DATABASE"
-    }
-  })
+  schedule = "cron(0 * * * ? *)"
 }
 
-# Athena workgroup
-resource "aws_athena_workgroup" "primary" {
-  name = "primary"
+# --- Athena WorkGroup (Custom) ---
+resource "aws_athena_workgroup" "custom_wg" {
+  name = "luffy_workgroup"
+
   configuration {
     result_configuration {
-      output_location = "s3://${var.bucket_name}/athena_logs/"
+      output_location = "s3://${aws_s3_bucket.superstore_bucket.bucket}/athena_logs/"
     }
   }
+
+  state = "ENABLED"
 }
